c88448be970ab0e68c697d662f52c10e2527c857
Nicolas <m26416088@john.petra.ac.id>
2018-10-17 19:34:54 +0800

    footer

--- home.php
+++ home.php
@@ -65,8 +65,8 @@
         </div>
 
 
-        <hr class="clearfix w-100 d-md-none pb-3">
-        <div class="col-md-3 mb-md-0 mb-3">
+      
+        <div class="col-md-6 ">
             <h5 class="text-uppercase">Links</h5>
 
             <ul class="list-unstyled">
@@ -85,25 +85,7 @@
             </ul>
 
           </div>
-          <div class="col-md-3 mb-md-0 mb-3">
-            <h5 class="text-uppercase">Links</h5>
-
-            <ul class="list-unstyled">
-              <li>
-                <a href="#!">Link 1</a>
-              </li>
-              <li>
-                <a href="#!">Link 2</a>
-              </li>
-              <li>
-                <a href="#!">Link 3</a>
-              </li>
-              <li>
-                <a href="#!">Link 4</a>
-              </li>
-            </ul>
-
-          </div>
+    
       </div>
     </div>
     <div class="footer-copyright text-center py-3">© 2018 Copyright:
