07fc7da1803d64843d0c6219927ed2cb633a13ac
Nicolas <m26416088@john.petra.ac.id>
2018-10-17 19:30:33 +0800

    home sementara

--- home.php
+++ home.php
@@ -6,6 +6,14 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <style type="text/css">
+  body{
+  	background-color: #2a2b2d; 
+  	color:#e6e9ef; 
+  	font: arial;
+  	text-decoration: none;
+  }
+  </style>
 <head>
 <nav class="navbar navbar-inverse">
   <div class="container-fluid">
@@ -14,7 +22,7 @@
     </div>
     <ul class="nav navbar-nav">
       <li><a href="#">Map</a></li>
-      <li><a href="#">About Us</a></li>
+      <li><a href="#">Services</a></li>
       <li class="dropdown">
         <a class="dropdown-toggle" data-toggle="dropdown" href="#">Login
         <span class="caret"></span></a>
@@ -27,15 +35,79 @@
   </div>
 </nav>
 </head>
-<body style="background-color: #2a2b2d; color:white;">
+<body>
 <div class="container-fluid">
 	Today's Schedule
 </div>
+
+    <section class="container-fluid" id="about">
+      <div class="container">
+        <div class="row">
+          <div class="col-rg-4 mx-auto text-center">
+            <h2 class="section-heading text-white">About Us</h2>
+            <hr class="light my-8">
+            <p class="text-faded mb-4">Menjawab Kebutuhan Transportasi Anda: Mobil, Ojek, Pengiriman Barang & Makanan. Banyak Pilihan Pembayaran. Aman & Terpercaya. Dilindungi Asuransi. Tarif Tetap. Pengemudi Terlatih. Services: GrabTaxi, GrabCar, GrabBike, GrabExpress, GrabFood, GrabShare, GrabHitch..</p>
+             <h2 class="section-heading text-white">-------------------------------------------------</h2>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
 </body>
 
-<footer class="page-footer font-small">
-  <div class="footer-copyright navbar-inverse text-center py-3">© 2018 Copyright:
-    <a href="home.php">IMK</a>
-  </div>
-</footer>
+<footer class="navbar-inverse page-footer font-small blue pt-4">
+    <div class="container-fluid text-center text-md-left">
+      <div class="row">
+        <div class="col-md-6 mt-md-0 mt-3">
+          <h5 class="text-uppercase">Footer Content</h5>
+          <p>Here you can use rows and columns here to organize your footer content.</p>
+        </div>
+
+
+        <hr class="clearfix w-100 d-md-none pb-3">
+        <div class="col-md-3 mb-md-0 mb-3">
+            <h5 class="text-uppercase">Links</h5>
+
+            <ul class="list-unstyled">
+              <li>
+                <a href="#!">Link 1</a>
+              </li>
+              <li>
+                <a href="#!">Link 2</a>
+              </li>
+              <li>
+                <a href="#!">Link 3</a>
+              </li>
+              <li>
+                <a href="#!">Link 4</a>
+              </li>
+            </ul>
+
+          </div>
+          <div class="col-md-3 mb-md-0 mb-3">
+            <h5 class="text-uppercase">Links</h5>
+
+            <ul class="list-unstyled">
+              <li>
+                <a href="#!">Link 1</a>
+              </li>
+              <li>
+                <a href="#!">Link 2</a>
+              </li>
+              <li>
+                <a href="#!">Link 3</a>
+              </li>
+              <li>
+                <a href="#!">Link 4</a>
+              </li>
+            </ul>
+
+          </div>
+      </div>
+    </div>
+    <div class="footer-copyright text-center py-3">© 2018 Copyright:
+      <a href="">IMK</a>
+    </div>
+  </footer>
 </html>
