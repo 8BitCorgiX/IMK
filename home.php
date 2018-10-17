1aef20bc9011fb942d958e91397ea338a74b2c25
Nicolas <m26416088@john.petra.ac.id>
2018-10-17 18:54:31 +0800

    Initial commit

---
+++ home.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<title>IMK</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<head>
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="home.php">IMK</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li><a href="#">Map</a></li>
+      <li><a href="#">About Us</a></li>
+      <li class="dropdown">
+        <a class="dropdown-toggle" data-toggle="dropdown" href="#">Login
+        <span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="#">Login</a></li>
+          <li><a href="#">Sign Up</a></li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+</nav>
+</head>
+<body>
+
+</body>
+</html>
